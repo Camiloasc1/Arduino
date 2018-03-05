27e9872fb7800bf002d88937d6f54de44e6c2eac
caasanchezcu <caasanchezcu@unal.edu.co>
2018-03-04 19:19:42 -0500

    Siren using a piezo buzzer

---
+++ siren/siren.ino
@@ -0,0 +1,34 @@
+const int PIN_BUZZER = 9;
+const int sirenPeriodms = 2000;
+const int sirenStepms = 1;
+const float sirenSteps = sirenPeriodms / sirenStepms / 2;
+const int sirenFreqMin = 500;
+const int sirenFreqMax = 1200;
+const float sirenFreqStep = (sirenFreqMax - sirenFreqMin) / sirenSteps;
+
+void setup() {
+  // Serial.begin(9600);
+  // Serial.println(0);
+  pinMode(PIN_BUZZER, OUTPUT);
+}
+
+void loop() {
+  float freq;
+
+  // Ramp up
+  for (int i = 0; i < sirenSteps; i++) {
+    freq = sirenFreqMin + sirenFreqStep * i;
+    tone(PIN_BUZZER, freq);
+    delay(sirenStepms);
+    // noTone(PIN_BUZZER);
+  }
+
+  // Ramp down
+  for (int i = 0; i < sirenSteps; i++) {
+    freq = sirenFreqMax - sirenFreqStep * i;
+    tone(PIN_BUZZER, freq);
+    delay(sirenStepms);
+    // noTone(PIN_BUZZER);
+  }
+}
+
